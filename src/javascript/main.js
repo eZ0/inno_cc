7d2fc3d9191af6f249ab49da864bbd68b1176374
Ksenia Karelskaya <kusksu@gmail.com>
2014-12-22 15:14:26 +0100

    centered links, slider on small screens, menu changes to X

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -1,32 +1,16 @@
 $( document ).ready(function() {
 
-//slide();
+	slide();
 
-//var el = document.getElementById('menu');
-var isDisplayed;
+	var changeClass = function(r, className1, className2) {
+		var regex = new RegExp("(?:^|\\s+)" + className1 + "(?:\\s+|$)");
 
-var changeDisplayAttribute = function(){
-	// if (isDisplayed) {
-	// 	el.style.display = 'block';
-	// 	//isDisplayed = false;
-	// 	console.log('block!!');
-	// }else{
-	// 	el.style.display = 'none';
-	// 	//isDisplayed = true;
-	// 	console.log('none!!');
-	// }
-};
+		console.log(r.className);
 
-var changeClass = function(r, className1, className2) {
-
-	var regex = new RegExp("(?:^|\\s+)" + className1 + "(?:\\s+|$)");
-
-	console.log(r.className);
-
-	if( regex.test(r.className) ) {
-		r.className = r.className.replace(regex,''+className2+'');
-		console.log('should change to none!');
-		isDisplayed = false;
+		if( regex.test(r.className) ) {
+			r.className = r.className.replace(regex,''+className2+'');
+			console.log('should change to none!');
+			isDisplayed = false;
 		//changeDisplayAttribute();
 	}
 	else{
@@ -46,34 +30,34 @@ var changeClass = function(r, className1, className2) {
 	// };
 
 	var toggled = false;
-   	var el = document.getElementById('menu');
-   	var close_icon = document.getElementById('close-icon');
-   	var menu_icon =  document.getElementById('menu-icon');
+	var el = document.getElementById('menu');
+	var close_icon = document.getElementById('close-icon');
+	var menu_icon =  document.getElementById('menu-icon');
 
-    	menu_icon.onclick = function() {
-      if( toggled ) {
-        	el.style.height = "1500px";
-           	el.style.display = "block";
-           	close_icon.style.display = "inline-block";
-           	//menu_icon.style.display = "none";
-        } else {
-        	//el.style.height = "0px";
-           	el.style.display = "none";
-        }
-        toggled = !toggled;
-    };
-    close_icon.onclick = function() {
-    		el.style.height = "0px";
-    		el.style.display = "none";
-    		close_icon.style.display = "none";
-    		//menu_icon.style.display = "inline-block";
-    };
+	menu_icon.onclick = function() {
+		if( toggled ) {
+			el.style.height = "1500px";
+			el.style.display = "block";
+			close_icon.style.display = "inline-block";
+			menu_icon.style.display = "none";
+		} else {
+			//el.style.height = "0px";
+			el.style.display = "none";
+		}
+		toggled = !toggled;
+	};
+	close_icon.onclick = function() {
+		el.style.height = "0px";
+		el.style.display = "none";
+		close_icon.style.display = "none";
+		menu_icon.style.display = "inline-block";
+	};
 
-    	document.getElementById('aside-nav').onclick = function() {
-    		$('#sidemenu').modal('toggle');
-    	};
-    	document.getElementById('sidemenu').onclick = function() {
-    		$('#sidemenu').modal('hide');
-    	};
+	document.getElementById('aside-nav').onclick = function() {
+		$('#sidemenu').modal('toggle');
+	};
+	document.getElementById('sidemenu').onclick = function() {
+		$('#sidemenu').modal('hide');
+	};
 
 });
